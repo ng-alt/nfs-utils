8b4eccdab33bc17ca815f1d11c53139179ba4752
Gerrit Pusher <pusher@localhost>
2003-07-18 07:40:58 +0800

    Import nfs-utils v1.0.5

--- utils/mountd/mountd.c
+++ utils/mountd/mountd.c
@@ -431,14 +431,14 @@ main(int argc, char **argv)
 	char	*export_file = _PATH_EXPORTS;
 	int	foreground = 0;
 	int	port = 0;
-	int	descriptors = 256;
+	int	descriptors = 0;
 	int	c;
 	struct sigaction sa;
 	struct rlimit rlim;
 
 	/* Parse the command line options and arguments. */
 	opterr = 0;
-	while ((c = getopt_long(argc, argv, "on:Fd:f:p:P:hN:V:v", longopts, NULL)) != EOF)
+	while ((c = getopt_long(argc, argv, "o:n:Fd:f:p:P:hN:V:v", longopts, NULL)) != EOF)
 		switch (c) {
 		case 'o':
 			descriptors = atoi(optarg);
@@ -498,19 +498,20 @@ main(int argc, char **argv)
 		exit(1);
 	}
 
-	if (getrlimit (RLIMIT_NOFILE, &rlim) != 0) {
-		fprintf(stderr, "%s: getrlimit (RLIMIT_NOFILE) failed: %s\n",
-			argv [0], strerror(errno));
-		exit(1);
-	}
+	if (descriptors) {
+		if (getrlimit (RLIMIT_NOFILE, &rlim) != 0) {
+			fprintf(stderr, "%s: getrlimit (RLIMIT_NOFILE) failed: %s\n",
+				argv [0], strerror(errno));
+			exit(1);
+		}
 
-	rlim.rlim_cur = descriptors;
-	if (setrlimit (RLIMIT_NOFILE, &rlim) != 0) {
-		fprintf(stderr, "%s: setrlimit (RLIMIT_NOFILE) failed: %s\n",
-			argv [0], strerror(errno));
-		exit(1);
+		rlim.rlim_cur = descriptors;
+		if (setrlimit (RLIMIT_NOFILE, &rlim) != 0) {
+			fprintf(stderr, "%s: setrlimit (RLIMIT_NOFILE) failed: %s\n",
+				argv [0], strerror(errno));
+			exit(1);
+		}
 	}
-
 	/* Initialize logging. */
 /*	xlog_open("mountd"); */
 
